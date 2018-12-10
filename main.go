02024e6f6acd87329a862d9d8e38fd79af02dbfc
pa_gorovoy <pa_gorovoy@bingo-boom.com>
2018-12-10 18:54:42 +0300

    la

--- main.go
+++ main.go
@@ -4,17 +4,9 @@ import (
 	core "bb_core"
 )
 
-type Aaa struct {
-	A string `json:"a,omitempty"`
-	B int    `json:"b,omitempty"`
-}
-
-var ss = Aaa{B: 1}
-
 func main() {
 	core.Data.Config.Init("./config.development.json")
 	// fmt.Printf("%+v\n", core.Data.Config)
-	// fmt.Printf("%+v", ss)
 
 	// src.RmqInit()
 	// go src.RedisInit()
