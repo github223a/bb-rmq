771226c69a6883a9769b0a42c797a04e4913ed8c
pa_gorovoy <pa_gorovoy@bingo-boom.com>
2018-12-11 18:59:06 +0300

    lala

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	core "bb_core"
-	"fmt"
 )
 
-var Config = &ConfigStructure{}
+/*Config lalala*/
+var Config ConfigStrucuture
 
 func main() {
 	core.Data.InitConfig()
 	// fmt.Printf("%+v\n", core.Data.Config)
 
-	// ddd, _ := json.Marshal(core.Data.Config)
-	// json.Unmarshal([]byte(ddd), &Config)
+	// bytes, _ := json.Marshal(core.Data.Config)
+	// json.Unmarshal([]byte(bytes), &Config)
 	// core.Data.Config = Config
 
-	fmt.Printf("%+v\n", core.Data.Config)
+	// fmt.Printf("Config ===== %+v\n", Config)
+	// fmt.Printf("ALLALA \n%s", core.Data.Config.Namespace)
 
 	// data, _ := json.MarshalIndent(core.Data.Config, ",", " ")
 	// fmt.Printf("%+v\n", string(data))
